7386220376e1318e46f47a4738a5df11f20b3154
Justin Shaytar <jas7650@rit.edu>
2024-05-15 14:51:22 -0400

    Checking in with current progress on frontend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-# from .models import Note
+from .models import Note
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,8 +15,8 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
-# class NoteSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Note
-#         fields = ["id", "title", "content", "created_at", "author"]
-#         extra_kwargs = {"author": {"read_only": True}}
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
